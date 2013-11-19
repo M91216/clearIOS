d505ecbe9000a7cd817d2de13faece9cb3a57eb9
Michael Bain <mbain@fullsail.edu>
2013-11-18 19:48:06 -0500

    Week 3

--- js/main.js
+++ js/main.js
@@ -1,87 +1,74 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+var music
+var playFn
+var stopFn
+var networkFn
+var locationFn
+var onSuccessFn
+var takePictureFn
+var alert
+var networkState
+var compassFn
+//pictureSource = navigator.camera.PictureSourceType;
+//destinationType = navigator.camera.DestinationType;
 
 
 
- /*document.addEventListener("deviceready", onDeviceReady, false);
- alert("it loaded");
- };
- 
+// Cordova is ready
+//
 function onDeviceReady() {
+ 	//$("#play").on("click", playFn);
+ 	//$("#stop").on("click", stopFn);
+ 	//$("#camera").on("click", takePictureFn);
+ 	$("#geo").on("click", locationFn);
+ 	$("#network").on("click",compassFn);
+ 	//$("#alert").on("click", alert);
+    alert("device is ready!");
+    
+    
+    
+ 	//checkConnection();
+}
+
+var locationFn = function(){
+    
+    navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+}
+function handle_geolocation_query(position){
+ 	var element = document.getElementById("geolocation");
+    
+ 	element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+ 	'Longitude: '          + position.coords.longitude             + '<br />' +
+ 	'Altitude: '           + position.coords.altitude              + '<br />' +
+ 	'Accuracy: '           + position.coords.accuracy              + '<br />' +
+ 	'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+ 	'Heading: '            + position.coords.heading               + '<br />' +
+ 	'Speed: '              + position.coords.speed                 + '<br />' +
+ 	'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+    
+}
+
+var compassFn = function(){
+
   
-  
-        alert("onDeviceReady");
-  
+navigator.compass.getCurrentHeading(onSuccess, onError);
 
-      };*/
-$(document).ready(function(){
-	
-	$("#sport").click(function(){
-	
-	
-	$.ajax({
-        url: "http://api.espn.com/v1/sports/news/headlines/",
-        data: {
-                // enter your developer api key here
-                apikey: "zhnt7m4bemtbqt73ypfg7svy",
-                // the type of data you're expecting back from the api
-                _accept: "application/json"
-        },
-        dataType: "jsonp",
-        success: function (data) {
-                // create an unordered list of headlines
-                var ul = $('<ul/>');
-                $.each(data.headlines, function () {
-                        var li = $('<li/>').text(this.headline);
-                        ul.append(li)
-                });
-                // append this list to the document body
-                $('#espn').append(ul);
-                
-        },
-});
-		
-		alert("Hello ESPN");
-		
-		
-		
-	});
-	
-	
-	
-});
+}
+function onSuccess(heading) {
+    alert('Heading: ' + heading.magneticHeading);
+}
 
-
-$("#insta").click(function(){
-
-var tag = "maschine";
-          var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=e79ad9e2173443deb8eea89ef21b15e9&amp;min_id=10";
-          $.getJSON(url, screenOutput);
-        });
-
-        var screenOutput = function(info){
-        
-        $("#data-msg").html("<h2>Search Results:</h2>");
-        
-        $.each(info.data, function(index, photo) {
-
-          var pic = "<li><img src ='" + photo.images.thumbnail.url + "' alt='" + photo.user.id
-                         + "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
-                         $("#data-output").append(pic);
-
-
-                });
-
-          };
+// onError: Failed to get the heading
+//
+function onError(compassError) {
+    alert('Compass Error: ' + compassError.code);
+}
 
 
 
-$(function(){
-	$(".clickme").mousedown(function(){      
-		$(this).addClass("selecionado");
-	});
 
-	$(".clickme").mouseup(function(){    
-		$(this).removeClass("selecionado");
-	});
-	
-});
+// device APIs are available
+//
+
 
